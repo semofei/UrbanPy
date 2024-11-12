50aaf0453dc112798dcbc75d1a21d5d571c6d070
Semofei <semofei@gmail.com>
2024-11-12 19:29:11 +0300

    fix module9_3

--- module9/module9_3.py
+++ module9/module9_3.py
@@ -2,7 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(word1)-len(word2)) for word1, word2 in zip(first, second) if len(word1) != len(word2))
-second_result = (len(first[i]) > len(second[i]) for i in range(0, len(first)) if len(first) >= len(second))
+second_result = (len(first[i]) <= len(second[i]) for i in range(0, len(first)) if len(first) >= len(second))
 
 print(list(first_result))
 print(list(second_result))
